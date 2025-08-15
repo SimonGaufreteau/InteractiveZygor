fab0b68ed53ac0248a1c165138c135420bd4839c
SimonGaufreteau <simon.gaufreteau@gmail.com>
2025-08-15 13:20:35 +0200

    Load and rescale points from points+zones csv

--- src/loader.ts
+++ src/loader.ts
@@ -1,16 +1,68 @@
 import { CSVToArray } from "./csv-parser";
+import type { Point, ZonePoint } from "../scripts/types";
 
-export async function loadPointsFromGuide(guide: string) {
+export async function loadPointsFromGuide(guide: string): Promise<ZonePoint[]> {
   const rawPoints: string[][] = CSVToArray(await (await fetch(guide)).text());
 
   // TODO : Remove header line in parser
   rawPoints.shift();
   rawPoints.pop();
 
-  const csvData = rawPoints.map((p) => ({
-    x: Number.parseFloat(p[0]),
-    y: Number.parseFloat(p[1]),
+  const csvData: ZonePoint[] = rawPoints.map((p) => ({
+    zone: p[0],
+    point: {
+      x: Number.parseFloat(p[1]),
+      y: Number.parseFloat(p[2]),
+    },
   }));
 
   return csvData;
+}
+
+export async function loadZones(zonesPath: string): Promise<Map<string, Zone>> {
+  const rawZones: string[][] = CSVToArray(
+    await (await fetch(zonesPath)).text(),
+  );
+  // TODO : Remove header line in parser
+  rawZones.shift();
+  rawZones.pop();
+
+  // TODO : Refactor this + duplicate function
+  const csvData: Zone[] = rawZones.map((z) => ({
+    name: z[0],
+    topl_y: Number.parseFloat(z[1]),
+    topl_x: Number.parseFloat(z[2]),
+    bright_y: Number.parseFloat(z[3]),
+    bright_x: Number.parseFloat(z[4]),
+    orig_y: Number.parseFloat(z[5]),
+    orig_x: Number.parseFloat(z[6]),
+  }));
+
+  const res = csvData.reduce((prev: Map<string, Zone>, current) => {
+    return prev.set(current.name, current);
+  }, new Map<string, Zone>());
+  return res;
+}
+
+export function rescalePointToMap(
+  zonePoint: ZonePoint,
+  zones: Map<string, Zone>,
+): Point | undefined {
+  const zone = zones.get(zonePoint.zone);
+  if (!zone) return undefined;
+  const point = zonePoint.point;
+
+  // Original scale : by how much we need to multiply the coordinates to get the pixel position
+  const orig_scale_x = zone.orig_x / 100;
+  const orig_scale_y = zone.orig_y / 100;
+  // New scale : how much bigger the new map is * the original scaling
+  const new_scale_x =
+    (orig_scale_x * Math.abs(zone.bright_x - zone.topl_x)) / zone.orig_x;
+  const new_scale_y =
+    (orig_scale_y * Math.abs(zone.bright_y - zone.topl_y)) / zone.orig_y;
+
+  return {
+    x: zone.topl_x + point.x * new_scale_x,
+    y: zone.topl_y - point.y * new_scale_y,
+  };
 }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "leaflet-polylinedecorator";
 import "./style.css";
 
 import { loadMap, setTriggers } from "./map_utils";
-import { loadPointsFromGuide } from "./loader";
+import { loadPointsFromGuide, loadZones, rescalePointToMap } from "./loader";
 
 function examplePoints(map: L.Map) {
   // Example points
@@ -41,26 +41,12 @@ const map = loadMap();
 examplePoints(map);
 setTriggers(map);
 
-const points = await loadPointsFromGuide("/points.csv");
+const zonePoints = await loadPointsFromGuide("/points.csv");
+const zones = await loadZones("/zones.csv");
+const rescaledPoints = zonePoints.map((zp) => rescalePointToMap(zp, zones));
 
-// Elwynn Forest coordinates
-const xTop = 8378;
-const yTop = 3908;
-const xBot = 10100;
-const yBot = 2888;
-
-// Original map size
-const orig_x = 1000;
-const orig_y = 668;
-// Original scale : by how much we need to multiply the coordinates to get the pixel position
-const orig_scale_x = orig_x / 100;
-const orig_scale_y = orig_y / 100;
-// New scale : how much bigger the new map is * the original scaling
-const new_scale_x = (orig_scale_x * Math.abs(xBot - xTop)) / orig_x;
-const new_scale_y = (orig_scale_y * Math.abs(yBot - yTop)) / orig_y;
-
-points
-  .map((p) => ({ x: xTop + p.x * new_scale_x, y: yTop - p.y * new_scale_y }))
+rescaledPoints
+  .filter((p) => p != undefined)
   .forEach((p) =>
     L.marker([p.y, p.x]).bindPopup(`Marker at ${p.x}, ${p.y}`).addTo(map),
   );
